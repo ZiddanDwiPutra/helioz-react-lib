6836afb6f37c503a17ef057a25cb1c03e5626f87
ZiddanStrife <49517837+ZiddanDwiPutra@users.noreply.github.com>
2026-04-27 11:15:54 +0700

    feat: initial features

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ insertTypesEntry: true }) // Menghasilkan file .d.ts
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'HeliozReactLib',
+      fileName: 'helioz-react-lib',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
